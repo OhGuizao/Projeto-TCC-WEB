cdb499c9f325a9554d67ad8d0d3696777247ed2f
Guilherme Santos <79462445+OhGuizao@users.noreply.github.com>
2023-11-12 23:04:03 -0300

    Rotas de usuário 100% finalizadas, faltando apenas terminar a documentação do swagger!

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,6 @@
 const { config } = require('dotenv');
-const mysql = require('mysql2/promise'); // Usando a versão Promise do mysql2
+const mysql = require('mysql2/promise');
+const Usuario = require('../model/User');
 
 config();
 
@@ -10,40 +11,109 @@ const clientDB = mysql.createPool({
     database: process.env.DATABASE
 });
 
-const User = require('../model/User');
-
 class UserController {
 
-
-    //PEGAR TODOS OS USUÁRIOS
+    // PEGAR TODOS OS USUÁRIOS
     async getAll(req, res) {
         try {
-            const [usuario] = await clientDB.query('SELECT * FROM tbusuario');
-            console.log(usuario);
-            res.status(200).json(usuario);
+            const [usuarios] = await clientDB.query('SELECT * FROM tbusuario');
+            res.status(200).json(usuarios);
         } catch (error) {
             console.error('Erro ao obter usuários:', error);
             res.status(500).json({ msg: 'Erro interno do servidor' });
         }
     }
 
-    async post(req, res) {
-        //validação
-        const { user, password, cpfUsuario } = req.body;
-        if (!user) {
-            return res.status(422).json({ msg: 'Usuário é obrigatório!' })
-        }
-        if (!password) {
-            return res.status(422).json({ msg: 'Senha é obrigatória!' })
-        }
-        if (!cpfUsuario) {
-            return res.status(422).json({ msg: 'Cpf é obrigatório!' })
-        }
+    async getPorCpf(req, res) {
+        try {
+            const { cpfUsuario } = req.params;
+            // Verificar se o usuário com o CPF fornecido existe
+            const [rows] = await clientDB.query('SELECT * FROM tbusuario WHERE cpfUsuario = ?', [cpfUsuario]);
 
-        //check if user exists
-        const userExists = await User.findOne({ cpfUsuario: cpfUsuario });
-        if(userExists){
-            return res.status(422).json({msg:'Este cpf já está registrado!'})
+            if (rows.length > 0) {
+                res.status(200).json(rows[0]);
+            } else {
+                res.status(404).json({ msg: 'Usuário não encontrado para o CPF fornecido.' });
+            }
+        } catch (error) {
+            console.error('Erro ao buscar usuário por CPF:', error);
+            res.status(500).json({ msg: 'Erro interno do servidor' });
+        }
+    }
+
+
+    // ADICIONAR NOVO USUÁRIO
+    async post(req, res) {
+        try {
+            // Validar dados recebidos
+            const { tipoUsuario, nomeUsuario, cpfUsuario, idadeUsuario, user, password } = req.body;
+            if (!tipoUsuario || !nomeUsuario || !cpfUsuario || !idadeUsuario || !user || !password) {
+                return res.status(422).json({ msg: 'Todos os campos são obrigatórios!' });
+            }
+            // Verificar se o usuário existe
+            const [rows] = await clientDB.query('SELECT * FROM tbusuario WHERE cpfUsuario = ?', [cpfUsuario]);
+            if (rows.length > 0) {
+                return res.status(422).json({ msg: 'Este CPF já está registrado!' });
+            }
+            // Inserir novo usuário
+            const [result] = await clientDB.execute(
+                'INSERT INTO tbusuario (tipoUsuario, nomeUsuario, cpfUsuario, idadeUsuario, user, password) VALUES (?, ?, ?, ?, ?, ?)',
+                [tipoUsuario, nomeUsuario, cpfUsuario, idadeUsuario, user, password]
+            );
+            const newUser = new Usuario(result.insertId, tipoUsuario, nomeUsuario, cpfUsuario, idadeUsuario, user, password);
+            res.status(201).json({ msg: 'Usuário cadastrado com sucesso!', user: newUser });
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+            res.status(500).json({ msg: 'Erro interno do servidor' });
+        }
+    }
+
+
+    // ATUALIZAR USUÁRIO
+    async put(req, res) {
+        // validação
+        const { idUsuario, tipoUsuario, nomeUsuario, cpfUsuario, idadeUsuario, user, password } = req.body;
+        console.log('ID do usuário recebido:', idUsuario);
+        if ( !tipoUsuario || !nomeUsuario || !cpfUsuario || !idadeUsuario || !user || !password) {
+            return res.status(422).json({ msg: 'Todos os campos são obrigatórios!' });
+        }
+        try {
+            // Verificar se o usuário existe
+            const [rows] = await clientDB.query('SELECT * FROM tbusuario WHERE idUsuario = ?', [idUsuario]);
+            console.log('Resultado da consulta ao banco de dados:', rows);
+            if (rows.length === 0) {
+                return res.status(404).json({ msg: 'Usuário não encontrado!' });
+            }
+            // Update user details
+            const [updatedUser] = await clientDB.query('UPDATE tbusuario SET tipoUsuario=?, nomeUsuario=?, idadeUsuario=?, user=?, password=? WHERE idUsuario=?',
+                [tipoUsuario, nomeUsuario, idadeUsuario, user, password, idUsuario]);
+
+            res.status(200).json({ msg: 'Usuário atualizado com sucesso!', updatedUser });
+        } catch (error) {
+            console.error('Erro ao atualizar usuário:', error);
+            res.status(500).json({ msg: 'Erro interno do servidor' });
+        }
+    }
+
+
+    // DELETAR USUÁRIO
+    async delete(req, res) {
+        const { idUsuario } = req.body;
+        if (!idUsuario) {
+            return res.status(422).json({ msg: 'ID do usuário é obrigatório!' });
+        }
+        try {
+            // Verificar se o usuário existe
+            const [rows] = await clientDB.query('SELECT * FROM tbusuario WHERE idUsuario = ?', [idUsuario]);
+            if (rows.length === 0) {
+                return res.status(404).json({ msg: 'Usuário não encontrado!' });
+            }
+            // Delete the user
+            const [deletedUser] = await clientDB.query('DELETE FROM tbusuario WHERE idUsuario=?', [idUsuario]);
+            res.status(200).json({ msg: 'Usuário deletado com sucesso!', deletedUser });
+        } catch (error) {
+            console.error('Erro ao deletar usuário:', error);
+            res.status(500).json({ msg: 'Erro interno do servidor' });
         }
     }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,11 @@ const userRoutes = express.Router();
 
 const userController = new UserController();
 
-userRoutes.get('/user',userController.getAll);
-userRoutes.post('/user',userController.post);
+userRoutes.get('/user', userController.getAll);
+userRoutes.get('/user/:cpfUsuario', userController.getPorCpf); // Correção: Adicionado ":" antes de cpfUsuario
+userRoutes.post('/user', userController.post);
+userRoutes.put('/user/:idUsuario', userController.put); // Correção: Adicionado ":" antes de idUsuario
+userRoutes.delete('/user/:idUsuario', userController.delete);
 
 // Outras rotas podem ser definidas aqui, se necessário
 
